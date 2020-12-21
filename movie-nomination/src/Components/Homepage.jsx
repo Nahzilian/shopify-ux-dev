27d851e02ec1be1fb28c6ac0603121d4747a3414
Doug <nahz3112@gmail.com>
2020-12-21 17:28:46 -0500

    change url

--- movie-nomination/src/Components/Homepage.jsx
+++ movie-nomination/src/Components/Homepage.jsx
@@ -4,7 +4,7 @@ import FadeIn from 'react-fade-in'
 import { ReactComponent as Award } from './assets/award.svg'
 import { ReactComponent as EmptyClip } from './assets/clip.svg'
 import { ReactComponent as Typing } from './assets/typing.svg'
-const baseAPI = `http://www.omdbapi.com/?apikey=9beeb635&s=`
+const baseAPI = `https://www.omdbapi.com/?apikey=9beeb635&s=`
 
 function onlyUnique(value, index, self) {
     return self.indexOf(value) === index;
